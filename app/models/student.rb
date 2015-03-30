b8e4fb0082526b2ed9034fe310df4b3a1bb6b738
Dmitry Sinina <dmitry.sinina@onat.edu.ua>
2015-03-30 15:21:24 +0300

    some new

--- app/models/student.rb
+++ app/models/student.rb
@@ -6,7 +6,7 @@ class Student < ActiveRecord::Base
   belongs_to :role,:class_name => StudentRole,:foreign_key => :role_id
   has_many :documents,:class_name => StudentUploadedDocument,:foreign_key => :student_id
 
-  validates_presence_of :first_name,:last_name,:group
+  validates_presence_of :first_name, :last_name, :group
   validates_format_of :email, :with => /(.*)@(.*)/
 
 
@@ -30,8 +30,9 @@ class Student < ActiveRecord::Base
         self.login=@try
       end
       super
-    end
+
     self.delay.ad_sync
+    end
   end
 
   def display_name
@@ -46,8 +47,46 @@ class Student < ActiveRecord::Base
 
     cont=ad.find_container('cn=Users')
     ad.load
-    user = RADUM::User.new :username => self.login,:container=>cont , :primary_group=>ad.find_group_by_name('Domain Users')
-    ad.sync
+
+    user = ad.find_user_by_username(self.login)
+    unless user.nil?
+      user.password=self.password
+      user.mail=self.email
+      user.telephone_number=self.phone
+      user.first_name=self.first_name
+      user.middle_name=self.middle_name
+      user.surname=self.last_name
+      ad.sync
+    else
+      user = RADUM::User.new :username => self.login,
+                           #:rid=>self.id,
+                           :container=>cont ,
+                           :primary_group=>ad.find_group_by_name('Domain Users'),
+                           :password=> self.password
+      ad.sync
+      self.ad_sync # fucking recursion
+    end
+
+
+   # user = User.find("Administrator")
+    #puts user
+
+    # ldap = Net::LDAP.new
+    # ldap.host = "10.4.1.101"
+    # ldap.port = 389
+    # ldap.authenticate "Administrator", "1yoyP23wru3hdgd"
+    #
+    # dn = "cn=#{self.login}, dc=test, dc=onat, dc=edu, dc=ua"
+    #
+    # attr = {
+    #     :cn => self.login,
+    #     :password => self.password,
+    #     :objectclass => "User",
+    #     :sn => self.login,
+    #     :telephoneNumber => self.phone,
+    #     :mail => self.email
+    # }
+    # ldap.add(:dn => dn, :attributes => attr)
 
   end
 
---
+++ app/models/user.rb
@@ -0,0 +1,6 @@
+class User < ActiveLdap::Base
+  ldap_mapping :classes => ['User'], :prefix => '', :dn_attribute=>'uid'
+
+
+end
+
